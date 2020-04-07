2ea17b3b5128635f3a129a7d5192e5e553eea020
TonioMG19 <toniokuhn2@gmail.com>
2020-04-07 17:05:41 +0200

    test

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ nbreClique = 0;
 bonus = 1;
 bonus2 = 1;
 prix=25;
+prix2=150;
 isAutoOn = false;
 
 
@@ -27,6 +28,12 @@ function assezCanelé(){
 	if(nbreClique<prix){
 		return false
 	}
+	if(nbreClique>=prix2){
+		return 'oui'
+	}
+	if(nbreClique<prix2){
+		return 'non'
+	}
 }
 
 function modification(){
@@ -35,6 +42,12 @@ function modification(){
 	}
 	if(!assezCanelé()){
 		document.getElementById("four").innerHTML = '<button disabled="on "id="four" onclick="achatfour()" >Acheter un four ('+prix+' canelés)</button>';
+	}
+	if(assezCanelé()=='oui'){
+		document.getElementById("auto").innerHTML='<button onclick="autoclick()">Auto-Click</button>';
+	}
+	if(assezCanelé()=='non'){
+		document.getElementById("auto").innerHTML='<button onclick="autoclick()" disabled="on">Auto-Click</button>';
 	}
 }
 
