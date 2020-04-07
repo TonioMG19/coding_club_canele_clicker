02e6303dcedd17efe95713ce5d85847bf2a42137
TonioMG19 <toniokuhn2@gmail.com>
2020-04-07 16:25:48 +0200

    test

--- script.js
+++ script.js
@@ -1,28 +1,41 @@
 nbreClique = 0;
 bonus = 1;
+prix=25;
 
 function clicktest() {
 	nbreClique= nbreClique + bonus;
-	document.getElementById("nbreCanelés").innerHTML = nbreClique;
-	document.getElementById("levelFour").innerHTML = bonus - 1;
-	if(nbreClique>=25){
-		document.getElementById("four").innerHTML = '<button id="four" onclick="achatfour()">Acheter un four (25 canelés)</button>'
-	}
-	if(nbreClique<25){
-		document.getElementById("four").innerHTML = '<button disabled="on "id="four" onclick="achatfour()">Acheter un four (25 canelés)</button>'
-	}
+	recharger();
+	modification();
 }
 
 function achatfour() {
 	nbreClique=nbreClique-25;
 	bonus++;
-	if(nbreClique>=25){
-		document.getElementById("four").innerHTML = '<button id="four" onclick="achatfour()" >Acheter un four (25 canelés)</button>'
+	prix = prix+5;
+	modification();
+	recharger();
+}
+
+
+function assezCanelé(){
+	if(nbreClique>=prix){
+		return true
 	}
-	if(nbreClique<25){
-		document.getElementById("four").innerHTML = '<button disabled="on "id="four" onclick="achatfour()" >Acheter un four (25 canelés)</button>'
+	if(nbreClique<prix){
+		return false
 	}
+}
+
+function modification(){
+	if(assezCanelé()){
+		document.getElementById("four").innerHTML = '<button id="four" onclick="achatfour()" >Acheter un four (25 canelés)</button>';
+	}
+	if(!assezCanelé()){
+		document.getElementById("four").innerHTML = '<button disabled="on "id="four" onclick="achatfour()" >Acheter un four (25 canelés)</button>';
+	}
+}
+
+function recharger(){
 	document.getElementById("nbreCanelés").innerHTML = nbreClique;
 	document.getElementById("levelFour").innerHTML = bonus - 1;
 }
-
