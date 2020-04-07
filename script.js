489ddaa62f00a9f1c390d5cc462410464ad60b2c
TonioMG19 <toniokuhn2@gmail.com>
2020-04-07 16:18:06 +0200

    etst

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ bonus = 1;
 function clicktest() {
 	nbreClique= nbreClique + bonus;
 	document.getElementById("nbreCanelés").innerHTML = nbreClique;
+	document.getElementById("levelFour").innerHTML = bonus - 1;
 	if(nbreClique>=25){
 		document.getElementById("four").innerHTML = '<button id="four" onclick="achatfour()">Acheter un four (25 canelés)</button>'
 	}
@@ -22,5 +23,6 @@ function achatfour() {
 		document.getElementById("four").innerHTML = '<button disabled="on "id="four" onclick="achatfour()" >Acheter un four (25 canelés)</button>'
 	}
 	document.getElementById("nbreCanelés").innerHTML = nbreClique;
+	document.getElementById("levelFour").innerHTML = bonus - 1;
 }
 
